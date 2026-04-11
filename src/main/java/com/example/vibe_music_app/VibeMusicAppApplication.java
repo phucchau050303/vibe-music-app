8f5d143f8fefb1edfcb309ed8dc6fe880479ed7d
Peter Hoang Chau <phcchu88@gmail.com>
2026-04-11 13:34:28 +0700

    initial creation

---
+++ src/main/java/com/example/vibe_music_app/VibeMusicAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.vibe_music_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeMusicAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibeMusicAppApplication.class, args);
+	}
+
+}
