03ffb6feb4446458a1b197eabc8e1ac2329e1bbe
Peter Hoang Chau <phcchu88@gmail.com>
2026-04-15 17:29:34 +0700

    build(playlist-fetcher): configure dependencies and test runtime

--- playlist-fetcher-service/src/main/java/com/music/fetch/config/PlaylistFetcherConfig.java
+++ playlist-fetcher-service/src/main/java/com/music/fetch/config/PlaylistFetcherConfig.java
@@ -1,4 +1,27 @@
 package com.music.fetch.config;
 
+import com.center.common.dto.MusicQuery;
+import com.center.common.dto.VibeResponse;
+import com.music.fetch.serivce.FetcherResponseService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+
+import java.util.List;
+import java.util.function.Function;
+
+@Configuration
 public class PlaylistFetcherConfig {
+    @Bean
+    public RestClient deezerRestClient(RestClient.Builder builder){
+        return builder.baseUrl("https://api.deezer.com").build();
+    }
+
+    @Bean
+    Function<MusicQuery, List<VibeResponse>> fetchPlaylists(FetcherResponseService fetcherResponseService) {
+        return fetcherResponseService::getPlaylists;
+    }
+
+
 }
