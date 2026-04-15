ae8ba169ddc69709fe3ca7c44c0e33c1031a6b62
Peter Hoang Chau <phcchu88@gmail.com>
2026-04-15 17:28:09 +0700

    feat(playlist-fetcher): add Deezer playlist fetch function

--- playlist-fetcher-service/src/main/java/com/music/fetch/DeezerFetcherApplication.java
+++ playlist-fetcher-service/src/main/java/com/music/fetch/DeezerFetcherApplication.java
@@ -1,4 +1,11 @@
 package com.music.fetch;
 
-public class MusicFetcherApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeezerFetcherApplication {
+    public static void main(String[] args){
+        SpringApplication.run(DeezerFetcherApplication.class, args);
+    }
 }
--- playlist-fetcher-service/src/main/java/com/music/fetch/serivce/FetcherResponseService.java
+++ playlist-fetcher-service/src/main/java/com/music/fetch/serivce/FetcherResponseService.java
@@ -1,4 +1,49 @@
 package com.music.fetch.serivce;
 
-public class FetcherReponse {
+import com.center.common.dto.DeezerPlaylistSearchResponse;
+import com.center.common.dto.MusicQuery;
+import com.center.common.dto.VibeResponse;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+
+@Service
+public class FetcherResponseService {
+    private final RestClient deezerRestClient;
+
+    public FetcherResponseService(RestClient deezerRestClient) {
+        this.deezerRestClient = deezerRestClient;
+    }
+
+    public List<VibeResponse> getPlaylists(MusicQuery query){
+            String genres = query.getGenres() == null ? "" : String.join(" ", query.getGenres());
+
+            String combinedSearchText = String.join(" ",
+                    query.getSearchQuery(),
+                    query.getMood(),
+                    genres
+            ).trim();
+            DeezerPlaylistSearchResponse response = deezerRestClient.get()
+                    .uri(uriBuilder -> uriBuilder
+                            .path("/search/playlist")
+                            .queryParam("q", combinedSearchText)
+                            .build())
+                    .retrieve()
+                    .body(DeezerPlaylistSearchResponse.class);
+
+            if (response == null || response.getData() == null){
+                return List.of();
+            }
+
+            return response.getData().stream()
+                    .map(item -> VibeResponse.builder()
+                            .title(item.getTitle())
+                            .playlistLink(item.getLink())
+                            .imageUrl(item.getPictureMedium())
+                            .platform("DEEZER")
+                            .description(item.getDescription())
+                            .build())
+                    .toList();
+    }
 }
